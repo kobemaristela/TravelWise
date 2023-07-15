f84106b2b9da049b7eb83e51ef1a54cf660050cc
Kobe Maristela <kobe@maristela.net>
2023-07-14 17:43:12 -0700

    Removed messages

--- travelwise/frontend/views.py
+++ travelwise/frontend/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
-from  django.contrib import messages
 
 # Create your views here.
 
@@ -58,5 +57,4 @@ def login_view(request):
 @login_required(login_url="landing")
 def logout_view(request):
     logout(request)
-    messages.info(request, "Logged out successfully!")
     return redirect("landing")
