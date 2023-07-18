c53360d757443184a44545c71e400a36e0bb2c04
Kobe Maristela <kobe@maristela.net>
2023-07-18 15:01:40 -0700

    Added TravelWise Base Models

--- travelwise/api/models.py
+++ travelwise/api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# API Models
+class TravelPlans(models.Model):
+    name = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    note = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+        def __unicode__(self):
+            return self.name
+
+
+class Activities(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    link = models.TextField()
+    note = models.TextField()
+    activity = models.ForeignKey(TravelPlans, on_delete=models.CASCADE)
+
+
+class Chat(models.Model):
+    time = models.TimeField()
+    user = models.TextField()
+    msg = models.IntegerField()
+    activity = models.ForeignKey(TravelPlans, on_delete=models.CASCADE)
