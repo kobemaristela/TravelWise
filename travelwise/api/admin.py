1c8d7894eae8d7569c0c518c9e246203d9565bc3
Kobe Maristela <kobe@maristela.net>
2023-07-22 17:31:50 -0700

    Added TravelWise Base Models (#18)

--- travelwise/api/admin.py
+++ travelwise/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TravelPlans, Activities, Chat
 
 # Register your models here.
+admin.site.register(TravelPlans)
+admin.site.register(Activities)
+admin.site.register(Chat)
