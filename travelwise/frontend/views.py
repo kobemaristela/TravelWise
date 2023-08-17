813c55db925df83d11d52ce8a2ec8afe3f0c55c8
Kobe Maristela <kobe@maristela.net>
2023-08-17 15:27:19 -0700

    Updated login error

--- travelwise/frontend/views.py
+++ travelwise/frontend/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
-from .forms import RegisterForm
+from django.contrib import messages
 from django.apps import apps
+from .forms import RegisterForm
+
 
 TravelPlan = apps.get_model('api', 'TravelPlan')
 ChatMessage = apps.get_model('api', 'ChatMessage')
@@ -63,6 +65,8 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 return redirect("home")
+        else:
+            messages.error(request, 'Invalid username or password')
 
     form = AuthenticationForm()
 
